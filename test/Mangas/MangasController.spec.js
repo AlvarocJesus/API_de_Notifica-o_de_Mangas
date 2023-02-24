9b62c87900f9cdc3a56d914484a220bac353a262
AlvarocJesus <alvim.coelho.jesus@gmail.com>
2023-02-24 09:38:05 -0300

    Adiciona modelo para scraping

--- test/Mangas/MangasController.spec.js
+++ test/Mangas/MangasController.spec.js
@@ -1,15 +1,15 @@
 const MangasService = require('../../src/mangas/mangas.service');
 
 describe('Manga Controller', () => {
-	const mangasService = new MangasService();
+  const mangasService = new MangasService();
 
-	describe('(POST) /mangas', () => {
-		it('should return successfully when created mangas in list user', async () => {
-			const manga = { nome: 'Titulo teste' };
+  describe('(POST) /mangas', () => {
+    it('should return successfully when created mangas in list user', async () => {
+      const manga = { nome: 'Titulo teste' };
 
-			const { body } = await mangasService.createManga(manga).expect(201);
+      const { body: mangaSaved } = await mangasService.createManga(manga).expect(201);
 
-			expect(body).toEqual(expect.ObjectContaining(manga));
-		});
-	});
+      expect(mangaSaved).toEqual(expect.ObjectContaining(manga));
+    });
+  });
 });
