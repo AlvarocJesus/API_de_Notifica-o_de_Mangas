e2528522c19d086afa53a8d8398f634ae5b7f36e
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2023-06-25 15:48:29 -0300

    Corrige scraping para verificar se existe novo capitulo de um manga

--- src/scrapping/puppeter.js
+++ src/scrapping/puppeter.js
@@ -1,6 +1,6 @@
 const Puppeteer = require('puppeteer');
 
-const mangaLinks = [
+/* const mangaLinks = [
 	'https://mangalivre.net/ler/orient/online/356979/capitulo-116#/!page0',
 	'https://mangalivre.net/ler/devouring-zone/online/361350/capitulo-100#/!page0',
 	'https://mangalivre.net/ler/buddha/online/365847/capitulo-6#/!page0',
@@ -24,24 +24,33 @@ const mangaLinks = [
 	'https://mangalivre.net/ler/shuumatsu-no-walkure/online/379398/62#/!page0',
 	'https://mangalivre.net/ler/seeking-the-flying-sword-path/online/375299/104#/!page0',
 	'https://mangalivre.net/ler/eleceed/online/170589/1#/!page0',
-];
+]; */
+
+// const link =
+// 	'https://mangalivre.net/ler/trash-of-the-counts-family/online/400465/92#/!page0';
+const link =
+	'https://mangalivre.net/ler/return-of-the-sss-class-ranker/online/471072/62#/!page0';
 
 async function Teste() {
-	const browser = await Puppeteer.launch({
-		headless: false,
-	});
+	const browser = await Puppeteer.launch();
 	const page = await browser.newPage();
 
-	// await page.waitForNavigation();
-	await page.goto(mangaLinks[1]);
+	await page.goto(link);
+	/* #reader-wrapper > div.reader-navigation.clear-fix > div.chapter-selection-container > div.chapter-next.disabled */
+	await page.waitForSelector('.chapter-next');
 
-	const actual_chapter = await page.evaluate(() => {
-		const nextChapter = document.querySelector('.chapter-next');
+	const teste = await page.$eval(
+		'.chapter-selection-container > .chapter-next',
+		(item) => {
+			return item.classList;
+		}
+	);
+	console.log(teste);
 
-		if (nextChapter.classList.contains('disabled')) return 'tem o disabled';
-	});
+	if (teste['1'] === 'disabled' /* .contains('disabled') */)
+		console.log('deu certo');
 
-	console.log(actual_chapter);
+	console.log('tem proximo capitulo');
 
 	await browser.close();
 }
