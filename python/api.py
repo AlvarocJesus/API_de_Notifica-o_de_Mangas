184bd650fdb1842328760b8178778e77a7cdde16
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-09-07 22:11:11 -0300

    Adiciona interface web para adicionar mangas

--- python/api.py
+++ python/api.py
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get('/')
 async def read_root():
   return {'Hello': 'World'}
+
+@app.get('/mangas')
+async def get_mangas():
+  return [{'title': 'One Piece'}, {'title': 'Naruto'}]
+
+@app.post('/mangas')
+async def save_manga(manga: dict):
+  return manga
