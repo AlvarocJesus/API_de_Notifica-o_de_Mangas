76af72758fc6622ae03017143f8cfd1450e1c67e
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-12-04 20:20:38 -0300

    Refatora tela de adicionar manga para usar pack em vez de grid e adiciona funcionalidade de alteração de texto baseado no tipo selecionado

--- front/desktop/AddManga.py
+++ front/desktop/AddManga.py
@@ -9,48 +9,56 @@ class AddManga:
 		root = Toplevel(parent)
 		root.title('Adicionar Manga')
 		# root.geometry('500x500')
-		addManga = ttk.Frame(root, padding=10)
-		addManga.grid()
+		# addManga = ttk.Frame(root, padding=10)
+		# addManga.grid()
 
-		ttk.Label(addManga, text='Hello').grid(column=0, row=0)
-		ttk.Button(addManga, text='Quit', command=root.destroy).grid(column=1, row=0)
+		ttk.Label(root, text='Hello').pack() #.grid(column=0, row=0)
+		ttk.Button(root, text='Quit', command=root.destroy).pack() #.grid(column=1, row=0)
 
 		tipo = StringVar()
-		ttk.Radiobutton(addManga, text='Manga', variable=tipo, value='Manga').grid(column=0, row=0)
-		ttk.Radiobutton(addManga, text='Anime', variable=tipo, value='Anime').grid(column=1, row=0)
-		ttk.Radiobutton(addManga, text='Filme', variable=tipo, value='Filme').grid(column=2, row=0)
-		# tipo = input('Indique se é um Anime, ou Manga, ou Filme: ') # 'Manga' ou 'Anime' ou 'Filme'
-		print(tipo.title())
-		print(tipo)
-		nomeLabel = ttk.Label(addManga, text=f'Digite o nome do {tipo}:').grid(column=0, row=1)
+		ttk.Radiobutton(root, text='Manga', variable=tipo, value='Manga', command=self.alterTextTipo).pack() #.grid(column=0, row=0)
+		ttk.Radiobutton(root, text='Anime', variable=tipo, value='Anime', command=self.alterTextTipo).pack() #.grid(column=1, row=0)
+		ttk.Radiobutton(root, text='Filme', variable=tipo, value='Filme', command=self.alterTextTipo).pack() #.grid(column=2, row=0)
+		print(tipo.get())
+
+		nomeLabel = ttk.Label(root, text=f'Digite o nome do {tipo}:').pack() #.grid(column=0, row=1)
 		print(nomeLabel)
-		nome = ttk.Entry(addManga).grid(column=1, row=1)
-		# nome = input(f'Digite o nome do {tipo}: ') # 'One Piece'
+		nameEntry = ttk.Entry(root)
+		nameEntry.pack() #.grid(column=1, row=1)
+		name = nameEntry.get()
+		print(name)
 
-		ttk.Label(addManga, text='Digite o episodio em que esta: ').grid(column=0, row=2)
-		nome = ttk.Entry(addManga).grid(column=1, row=2)
-		# cap_atual = int(input('Digite o episodio em que esta: ')) # 1000
+		ttk.Label(root, text='Digite o episodio em que esta: ').pack() #.grid(column=0, row=2)
+		nome = ttk.Entry(root).pack() #.grid(column=1, row=2)
 
-		ttk.Label(addManga, text='Digite o total de episodios: ').grid(column=0, row=3)
-		nome = ttk.Entry(addManga).grid(column=1, row=3)
-		# total_caps = int(input('Digite o total de episodios: ')) # 1000
+		ttk.Label(root, text='Digite o total de episodios: ').pack() #.grid(column=0, row=3)
+		nome = ttk.Entry(root).pack() #.grid(column=1, row=3)
 
-		ttk.Label(addManga, text='Digite a temporada em que esta: ').grid(column=0, row=4)
-		nome = ttk.Entry(addManga).grid(column=1, row=4)
-		# temp_atual = int(input('Digite a temporada em que esta: ')) # ''
+		ttk.Label(root, text='Digite a temporada em que esta: ').pack() #.grid(column=0, row=4)
+		nome = ttk.Entry(root).pack() #.grid(column=1, row=4)
 
-		ttk.Label(addManga, text='Digite o total de temporadas: ').grid(column=0, row=5)
-		nome = ttk.Entry(addManga).grid(column=1, row=5)
-		# temp_total = int(input('Digite o total de temporadas: ')) # ''
+		ttk.Label(root, text='Digite o total de temporadas: ').pack() #.grid(column=0, row=5)
+		nome = ttk.Entry(root).pack() #.grid(column=1, row=5)
 
-		ttk.Label(addManga, text='Digite a url onde esta acompanhando:').grid(column=0, row=6)
-		nome = ttk.Entry(addManga).grid(column=1, row=6)
-		# url = input('Digite a url onde esta acompanhando: ') # 'https://onepieceex.net/manga/one-piece-capitulo-1000/'
+		ttk.Label(root, text='Digite a url onde esta acompanhando:').pack() #.grid(column=0, row=6)
+		nome = ttk.Entry(root).pack() #.grid(column=1, row=6)
 
-		ttk.Label(addManga, text=f'Marque o status do {tipo}')
+		ttk.Label(root, text=f'Marque o status do {tipo}')
 		status_manga = StringVar()
-		ttk.Radiobutton(addManga, text='Em Andamento', variable=status_manga, value='Em Andamento').grid(column=0, row=7)
-		ttk.Radiobutton(addManga, text='Finalizado', variable=status_manga, value='Finalizado').grid(column=1, row=7)
-		# status = input(f'Digite o status do {tipo}, como Em Andamento ou Finalizado: ') # 'Em andamento'
+		ttk.Radiobutton(root, text='Em Andamento', variable=status_manga, value='Em Andamento').pack() #.grid(column=0, row=7)
+		ttk.Radiobutton(root, text='Finalizado', variable=status_manga, value='Finalizado').pack() #.grid(column=1, row=7)
 
-		ttk.Button(addManga, text='Adicionar', command=root.destroy).grid(column=1, row=8)
+		ttk.Button(root, text='Adicionar', command=root.destroy).pack() #.grid(column=1, row=8)
+	
+	def alterTextTipo(self):
+		print(self.tipo.get())
+		if self.tipo.get() == 'Manga':
+			self.nomeLabel.config(text='Digite o nome do Manga:')
+		elif self.tipo.get() == 'Anime':
+			self.nomeLabel.config(text='Digite o nome do Anime:')
+		elif self.tipo.get() == 'Filme':
+			self.nomeLabel.config(text='Digite o nome do Filme:')
+		else:
+			self.nomeLabel.config(text='Digite o nome do:')
+		print(self.nomeLabel)
+		print(self.nomeLabel.cget('text'))
