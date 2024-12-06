4dad72475e14723d629c997b333683728b8a0040
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-12-05 21:13:57 -0300

    Adiciona classe OldiSussy para extrair dados de mangás, organiza codigo para funcoes e formata a saída em JSON

--- scrap/oldiSussy.py
+++ scrap/oldiSussy.py
@@ -1,29 +1,51 @@
-import requests
 import bs4
+import json
+import requests
+from unidecode import unidecode
 
-# url = 'https://www.amazon.com.br/Monitor-Gamer-AOC-FreeSync-Q27G2/dp/B0C6FHZW5C'
-url = 'https://oldi.sussytoons.com/manga/logando-10-000-anos-no-futuro/'
+class OldiSussy:
+	def __init__(self):
+		print('OldiSussy')
 
-test = requests.get(url)
-soup = bs4.BeautifulSoup(test.text, 'html.parser')
+	url = 'https://oldi.sussytoons.com/manga/logando-10-000-anos-no-futuro/'
 
-with open('teste.html', 'w', encoding='utf-8') as file:
-	file.write(soup.prettify())
+	"""  url = 'https://www.amazon.com.br/Monitor-Gamer-AOC-FreeSync-Q27G2/dp/B0C6FHZW5C'
+	preço amazon
+	print(soup.title)
+	print(soup.find('span', class_="a-price").find('span', class_="a-offscreen")) """
 
-print(soup.title)
-print(soup.find('div', class_='post-title').find('h1').text.strip())
-capitulos = soup.find('ul', class_='main version-chap no-volumn').find_all('li')
-ultimo_capitulo = soup.find('ul', class_='main version-chap no-volumn').find_all('li')[0].find('a').text.strip()
-ultimo_capitulo_link = soup.find('ul', class_='main version-chap no-volumn').find_all('li')[0].find('a')['href']
-# print(capitulos)
-print(ultimo_capitulo)
-# print(ultimo_capitulo_link)
+	def get_data(self, url):
+		try:
+			html = requests.get(url)
+			soup = bs4.BeautifulSoup(html.text, 'html.parser')
 
-for capitulo in capitulos:
-	cap = capitulo.find('a')
-	# print(f'Capitulo: {cap.text}')
-	# print(f'Link: {cap["href"]}')
+			with open('teste.html', 'w', encoding='utf-8') as file:
+				file.write(soup.prettify())
+			
+			return soup
+		except Exception as e:
+			print(f'Error: {e}')
 
-""" preço amazon
-print(soup.title)
-print(soup.find('span', class_="a-price").find('span', class_="a-offscreen")) """
+	def extract_data(self, soup):
+		try:
+			titulo = soup.find('div', class_='post-title').find('h1').text.strip()
+			capitulo_mais_novo = soup.find('ul', class_='main version-chap no-volumn').find_all('li')[0].find('a').text.strip()
+			ultimo_capitulo_link = soup.find('ul', class_='main version-chap no-volumn').find_all('li')[0].find('a')['href']
+
+			print(f'Titulo: {titulo}')			
+			print(f'Capitulo: {capitulo_mais_novo}')
+			print(f'Link: {ultimo_capitulo_link}')
+
+			return { 'titulo': titulo, 'capitulo_mais_novo': unidecode(capitulo_mais_novo), 'ultimo_capitulo_link': ultimo_capitulo_link }
+		except Exception as e:
+			print(f'Error: {e}')
+	
+	def format_data(self, data):
+		print(f'Data: {json.dumps(data, indent=2)}')
+
+	def run(self):
+		soup = self.get_data(self.url)
+		data = self.extract_data(soup)
+		self.format_data(data)
+
+OldiSussy().run()
