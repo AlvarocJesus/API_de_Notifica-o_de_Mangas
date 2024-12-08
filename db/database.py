b9d97b3fd86694936f20369b0f4e2ed8d32dfa56
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-12-08 20:19:39 -0300

    Renomeia arquivo main.py para database.py e remove comentários desnecessários do arquivo .env

--- db/database.py
+++ db/database.py
@@ -1,10 +1,10 @@
 class Database:
 	def __init__(self):
 		print('Database')
-	
+
 	def addManga(self, manga):
 		print('Add manga')
 		print(f'Data Manga: {manga}')
-	
+
 	def getManga(self):
 		print('Get manga')
