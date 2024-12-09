a8d6c3a58c55d1b2f62e03b2e3b6109f0fcd9abd
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-12-08 21:28:53 -0300

    Adiciona classe Log com métodos para inicialização e registro de logs

--- config/log/log.py
+++ config/log/log.py
@@ -1,3 +1,40 @@
+import logging
+
 class Log:
+	encoding = ''
+	level = ''
+	logFormat = ''
+	datefmt = ''
+	
 	def __init__(self):
 		print('Log')
+		self.encoding = 'utf-8'
+		self.level = logging.DEBUG
+		self.logFormat = '%(asctime)s | %(levelname)s | %(message)s'
+		self.datefmt='%m/%d/%Y %I:%M:%S %p'
+	
+	def initLog(self, fileName):
+		print('initLog')
+		logger = logging.getLogger(__name__)
+		logging.basicConfig(filename=fileName, encoding = self.encoding, level = self.level, format=self.logFormat, datefmt=self.datefmt)
+
+		return logger
+	
+	def log(self, logger, level, msg):
+		print('log')
+		match level:
+			case 'debug':
+				print('debug')
+				logger.debug(msg)
+			case 'info':
+				print('info')
+				logger.info(msg)
+			case 'warning':
+				print('warning')
+				logger.warning(msg)
+			case 'error':
+				print('error')
+				logger.error(msg)
+			case 'critical':
+				print('critical')
+				logger.critical(msg)
