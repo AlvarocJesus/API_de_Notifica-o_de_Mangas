773611d41339a21da553343b1c8c7eca74dba34f
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-12-14 12:41:27 -0300

    Adiciona logging à classe Database para registro de operações e erros

--- db/database.py
+++ db/database.py
@@ -1,3 +1,6 @@
+import psycopg2
+from config.log.log import Log
+
 class Database:
 	def __init__(self):
 		print('Database')
@@ -6,17 +9,47 @@ class Database:
 		print('Add manga')
 		print(f'Data Manga: {manga}')
 
-		engine = self.getEngine()
+		conn, cursor = self.getEngine()
 
-		with engine.connect() as conn:
-			print('Connection')
+		try:
+			# with engine.connect() as conn:
+			# 	print('Connection')
+			cursor.execute('insert into manga (name, author, release_date) values (%s, %s, %s)')
+			conn.commit()
+			conn.close()
+
+			Log().log('info', 'Manga added')
+			return True
+		except Exception as e:
+			Log().log('error', e)
 
 	def getManga(self):
 		print('Get manga')
-		engine = self.getEngine()
+		conn, cursor = self.getEngine()
+		# engine = self.getEngine()
 
-		with engine.connect() as conn:
-			print('Connection')
+		# with engine.connect() as conn:
+		# 	print('Connection')
+
+		try:
+			# with engine.connect() as conn:
+			# 	print('Connection')
+			mangas = cursor.execute('select * from manga with(nolock)')
+			conn.commit()
+			conn.close()
+
+			Log().log('info', 'Manga added')
+			return mangas
+		except Exception as e:
+			Log().log('error', e)
 	
 	def getEngine(self):
-		print('Get engine')
+		try:
+			conn = psycopg2.connect('dbname=teste user=teste password=teste')
+			cursor = conn.cursor()
+
+			Log().log('info', 'Connection with database')
+			return conn, cursor
+		except Exception as e:
+			Log().log('error', e)
+		
