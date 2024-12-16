f76df223c16bb666b62f43a937ae11277de7259c
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-12-16 20:25:44 -0300

    Remove unnecessary print statements and commented code in Database class methods

--- db/database.py
+++ db/database.py
@@ -6,14 +6,10 @@ class Database:
 		print('Database')
 
 	def addManga(self, manga):
-		print('Add manga')
 		print(f'Data Manga: {manga}')
-
 		conn, cursor = self.getEngine()
 
 		try:
-			# with engine.connect() as conn:
-			# 	print('Connection')
 			cursor.execute('insert into manga (name, author, release_date) values (%s, %s, %s)')
 			conn.commit()
 			conn.close()
@@ -26,14 +22,8 @@ class Database:
 	def getAllManga(self):
 		print('Get manga')
 		conn, cursor = self.getEngine()
-		# engine = self.getEngine()
-
-		# with engine.connect() as conn:
-		# 	print('Connection')
 
 		try:
-			# with engine.connect() as conn:
-			# 	print('Connection')
 			mangas = cursor.execute('select * from manga with(nolock)')
 			conn.commit()
 			conn.close()
