2d8bdca0c31b50b3b033b2ff94eccaf88aa92bc5
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-12-29 15:18:52 -0300

    Refactor MangaOnline class to improve logging, add URL parameter to get_data method, and implement data extraction and saving functionality.

--- scrap/MangaOnline.py
+++ scrap/MangaOnline.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import bs4
 import json
 import requests
@@ -5,24 +11,45 @@ from unidecode import unidecode
 from config.log.log import Log
 
 class MangaOnline:
-	def __init__(self):
-		Log().initLog('mangaOnline.log')
+	logger = None
+	url = 'https://mangaonline.blog/manga/solo-leveling-ragnarok/'
 
-	def get_data(self):
+	def __init__(self):
+		self.logger = Log().initLog('mangaOnline_blog.log')
+
+	def get_data(self, url):
 		try:
-			Log().log('info', 'Data retrieved successfully')
-			return None
+			html = requests.get(url)
+			soup = bs4.BeautifulSoup(html.text, 'html.parser')
+
+			with open('teste.html', 'w', encoding='utf-8') as file:
+				file.write(soup.prettify())
+
+			Log().log(self.logger, 'info', 'Data retrieved successfully')
+			return soup
 		except Exception as e:
-			Log().log('error', f'Error: {e}')
+			Log().log(self.logger, 'error', f'Error: {e}')
 	
-	def extract_data(self):
+	def extract_data(self, soup):
 		try:
-			Log().log('info', 'Data extracted successfully')
+			titulo = soup.find('div', class_='post-title').find('h1').text.strip()
+			capitulo_recente = soup.find('div', class_='main version-chap no-volumn active').find_all('li')[0].find('a').text.strip().split(' ')[-1]
+			ultimo_capitulo_link = soup.find('ul', class_='main version-chap no-volumn active').find_all('li')[0].find('a')['href']
+
+			print(f'Titulo: {titulo}')
+			# print(f'Capitulo: {capitulo_recente}')
+			# print(f'Link: {ultimo_capitulo_link}')
+
+			Log().log(self.logger, 'info', 'Data extracted successfully')
 		except Exception as e:
-			Log().log('error', f'Error: {e}')
+			Log().log(self.logger, 'error', f'Error: {e}')
 
 	def format_data(self):
 		pass
 
 	def run(self):
-		pass
+		soup = self.get_data(self.url)
+		self.extract_data(soup)
+
+if __name__ == '__main__':
+	MangaOnline().run()
